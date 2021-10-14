67c47e50f786b770681e53bf63ee052f707ee63e
FatAlien <vladislav.chigrinov@gmail.com>
2021-10-14 12:57:49 +0300

    Create admin model

---
+++ AchiveClub/Shared/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace AchiveClub.Shared.Models
+{
+    class Admin
+    {
+        [BsonId]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Key { get; set; }
+    }
+}
