245a029e28a9244560193ad434bad0b7212eb7d4
Vlad Fedorovich <fedorovichvlad2007@gmail.com>
2021-11-24 23:01:03 +0300

    Create / Update
    Created AdminKeyModal.razor.css.
    Updated CurrentUser.razor.css
    Updated CurrentUser.razor
    Uncommented Database.EnsureDeleted(); in ApplicationContext.cs

--- AchiveClub/Server/ApplicationContext.cs
+++ AchiveClub/Server/ApplicationContext.cs
@@ -14,7 +14,7 @@ public class ApplicationContext : DbContext
 
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
     {
-        //Database.EnsureDeleted();
+        Database.EnsureDeleted();
         Database.EnsureCreated();
     }
 
