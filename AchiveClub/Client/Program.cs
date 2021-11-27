c95e5e535328781c4c041841abb1dd0b4ab23048
FatAlien <vladislav.chigrinov@gmail.com>
2021-11-28 01:11:52 +0300

    Add registration

--- AchiveClub/Client/Program.cs
+++ AchiveClub/Client/Program.cs
@@ -20,6 +20,7 @@ namespace AchiveClub.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<LoginService>();
+            builder.Services.AddScoped<RegistrationService>();
 
             builder.Services.AddBlazoredModal();
 
---
+++ AchiveClub/Client/Services/RegistrationService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using AchiveClub.Shared.DTO;
+
+namespace AchiveClub.Client
+{
+    public class RegistrationService
+    {
+        private HttpClient _client;
+
+        public RegistrationService(HttpClient httpClient)
+        {
+            _client = httpClient;
+        }
+        public async Task<bool> Registrate(RegisterParams registerParams)
+        {
+            var result = await _client.PostAsJsonAsync<RegisterParams>("/api/Auth/register", registerParams);
+            return result.IsSuccessStatusCode;
+        }
+    }
+}
