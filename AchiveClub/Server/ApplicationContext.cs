bb318dbe1ba564f3dffd2bf54e5afd2f8bc7c2fd
FatAlien <vladislav.chigrinov@gmail.com>
2021-12-30 00:29:05 +0300

    Switch to dotnet 5

--- AchiveClub/Server/ApplicationContext.cs
+++ AchiveClub/Server/ApplicationContext.cs
@@ -4,73 +4,74 @@ using System;
 using Bogus;
 using System.Linq;
 
-namespace AchiveClub.Server;
-public class ApplicationContext : DbContext
+namespace AchiveClub.Server
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Achievement> Achivements { get; set; }
-    public DbSet<Admin> Admins { get; set; }
-    public DbSet<Supervisor> Supervisors { get; set; }
-    public DbSet<CompletedAchievement> CompletedAchivements { get; set; }
-    public DbSet<Club> Clubs { get; set; }
-
-    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    public class ApplicationContext : DbContext
     {
-        //Database.EnsureDeleted();
-        Database.EnsureCreated();
-    }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Achievement> Achivements { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Supervisor> Supervisors { get; set; }
+        public DbSet<CompletedAchievement> CompletedAchivements { get; set; }
+        public DbSet<Club> Clubs { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        int userIdCounter = 1,
-            adminIdCounter = 1,
-            supervisorIdCounter = 1,
-            achiveIdCounter = 1,
-            completedAchiveIdCounter = 1,
-            clubsIdCounter = 1;
-
-        int usersCount = 20,
-            achiveCount = 10,
-            adminsCount = 3,
-            supervisorsCount = 5,
-            completedAchiveCount = 100,
-            clubsCount = 5;
-
-        var userFaker = new Faker<User>("ru")
-            .RuleFor(u => u.Id, f => userIdCounter++)
-            .RuleFor(u => u.FirstName, f => f.Person.FirstName)
-            .RuleFor(u => u.LastName, f => f.Person.LastName)
-            .RuleFor(u => u.Email, f => f.Person.Email)
-            .RuleFor(u => u.Avatar, f => $"/image/avatars/{f.Random.Number(1, 16)}.jpg")
-            .RuleFor(u => u.Password, f => f.Internet.Password(6))
-            .RuleFor(u => u.ClubRefId, f => f.Random.Number(1, clubsCount));
-
-        var adminFaker = new Faker<Admin>("ru")
-            .RuleFor(u => u.Id, f => adminIdCounter++)
-            .RuleFor(a => a.Name, f => f.Person.FullName)
-            .RuleFor(a => a.Password, f => f.Internet.Password(8));
-
-        var supervisorFaker = new Faker<Supervisor>("ru")
-            .RuleFor(u => u.Id, f => supervisorIdCounter++)
-            .RuleFor(a => a.Name, f => f.Person.FullName)
-            .RuleFor(a => a.Key, f => f.Internet.Password(8));
-
-        var achiveFaker = new Faker<Achievement>("ru")
-            .RuleFor(u => u.Id, f => achiveIdCounter++)
-            .RuleFor(a => a.Xp, f => f.Random.Number(5, 100) * 10)
-            .RuleFor(a => a.Title, f => $"{f.Hacker.Verb()} {f.Hacker.Adjective()} {f.Hacker.Noun()}")
-            .RuleFor(a => a.Description, f => f.Hacker.Phrase())
-            .RuleFor(a => a.LogoURL, f => f.Image.PicsumUrl(600, 600, imageId: f.Random.Number(1, 100)));
-
-        var comletedAchiveFaker = new Faker<CompletedAchievement>("ru")
-            .RuleFor(a => a.Id, f => completedAchiveIdCounter++)
-            .RuleFor(a => a.AchiveId, f => f.Random.Number(1, achiveCount))
-            .RuleFor(a => a.UserId, f => f.Random.Number(1, usersCount))
-            .RuleFor(a => a.SupervisorId, f => f.Random.Number(1, adminsCount))
-            .RuleFor(a => a.DateOfCompletion, f => f.Date.Recent(60));
-
-        string[] clubTitles =
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            int userIdCounter = 1,
+                adminIdCounter = 1,
+                supervisorIdCounter = 1,
+                achiveIdCounter = 1,
+                completedAchiveIdCounter = 1,
+                clubsIdCounter = 1;
+
+            int usersCount = 20,
+                achiveCount = 10,
+                adminsCount = 3,
+                supervisorsCount = 5,
+                completedAchiveCount = 100,
+                clubsCount = 5;
+
+            var userFaker = new Faker<User>("ru")
+                .RuleFor(u => u.Id, f => userIdCounter++)
+                .RuleFor(u => u.FirstName, f => f.Person.FirstName)
+                .RuleFor(u => u.LastName, f => f.Person.LastName)
+                .RuleFor(u => u.Email, f => f.Person.Email)
+                .RuleFor(u => u.Avatar, f => $"/image/avatars/{f.Random.Number(1, 16)}.jpg")
+                .RuleFor(u => u.Password, f => f.Internet.Password(6))
+                .RuleFor(u => u.ClubRefId, f => f.Random.Number(1, clubsCount));
+
+            var adminFaker = new Faker<Admin>("ru")
+                .RuleFor(u => u.Id, f => adminIdCounter++)
+                .RuleFor(a => a.Name, f => f.Person.FullName)
+                .RuleFor(a => a.Password, f => f.Internet.Password(8));
+
+            var supervisorFaker = new Faker<Supervisor>("ru")
+                .RuleFor(u => u.Id, f => supervisorIdCounter++)
+                .RuleFor(a => a.Name, f => f.Person.FullName)
+                .RuleFor(a => a.Key, f => f.Internet.Password(8));
+
+            var achiveFaker = new Faker<Achievement>("ru")
+                .RuleFor(u => u.Id, f => achiveIdCounter++)
+                .RuleFor(a => a.Xp, f => f.Random.Number(5, 100) * 10)
+                .RuleFor(a => a.Title, f => $"{f.Hacker.Verb()} {f.Hacker.Adjective()} {f.Hacker.Noun()}")
+                .RuleFor(a => a.Description, f => f.Hacker.Phrase())
+                .RuleFor(a => a.LogoURL, f => f.Image.PicsumUrl(600, 600, imageId: f.Random.Number(1, 100)));
+
+            var comletedAchiveFaker = new Faker<CompletedAchievement>("ru")
+                .RuleFor(a => a.Id, f => completedAchiveIdCounter++)
+                .RuleFor(a => a.AchiveId, f => f.Random.Number(1, achiveCount))
+                .RuleFor(a => a.UserId, f => f.Random.Number(1, usersCount))
+                .RuleFor(a => a.SupervisorId, f => f.Random.Number(1, adminsCount))
+                .RuleFor(a => a.DateOfCompletion, f => f.Date.Recent(60));
+
+            string[] clubTitles =
+            {
             "Дворец",
             "Чикаго",
             "Серебронск",
@@ -79,49 +80,50 @@ public class ApplicationContext : DbContext
             "Киберфлот"
         };
 
-        var clubFaker = new Faker<Club>("ru")
-            .RuleFor(a => a.Id, f => clubsIdCounter++)
-            .RuleFor(a => a.Title, f => f.PickRandom(clubTitles))
-            .RuleFor(a => a.Description, f => f.Lorem.Sentences(4))
-            .RuleFor(a => a.Address, f => f.Address.FullAddress())
-            .RuleFor(a => a.LogoURL, f => "image/dvorec_logo.png");
+            var clubFaker = new Faker<Club>("ru")
+                .RuleFor(a => a.Id, f => clubsIdCounter++)
+                .RuleFor(a => a.Title, f => f.PickRandom(clubTitles))
+                .RuleFor(a => a.Description, f => f.Lorem.Sentences(4))
+                .RuleFor(a => a.Address, f => f.Address.FullAddress())
+                .RuleFor(a => a.LogoURL, f => "image/dvorec_logo.png");
 
-        var users = userFaker.Generate(usersCount);
+            var users = userFaker.Generate(usersCount);
 
-        users.Add(new User
-        {
-            Email = "test@mail.com",
-            Password = "1234",
-            FirstName = "Алёша",
-            LastName = "Попович",
-            Avatar = "/image/avatars/8.jpg",
-            Id = userIdCounter++,
-            ClubRefId = 1
-        });
+            users.Add(new User
+            {
+                Email = "test@mail.com",
+                Password = "1234",
+                FirstName = "Алёша",
+                LastName = "Попович",
+                Avatar = "/image/avatars/8.jpg",
+                Id = userIdCounter++,
+                ClubRefId = 1
+            });
 
-        var admins = adminFaker.Generate(adminsCount);
+            var admins = adminFaker.Generate(adminsCount);
 
-        admins.Add(new Admin
-        {
-            Name = "admin",
-            Password = "admin",
-            Id = adminIdCounter++
-        });
+            admins.Add(new Admin
+            {
+                Name = "admin",
+                Password = "admin",
+                Id = adminIdCounter++
+            });
 
-        var supervisors = supervisorFaker.Generate(supervisorsCount);
+            var supervisors = supervisorFaker.Generate(supervisorsCount);
 
-        supervisors.Add(new Supervisor
-        {
-            Name = "god",
-            Key = "qwerty",
-            Id = supervisorIdCounter++
-        });
+            supervisors.Add(new Supervisor
+            {
+                Name = "god",
+                Key = "qwerty",
+                Id = supervisorIdCounter++
+            });
 
-        modelBuilder.Entity<Club>().HasData(clubFaker.Generate(clubsCount));
-        modelBuilder.Entity<User>().HasData(users);
-        modelBuilder.Entity<Admin>().HasData(admins);
-        modelBuilder.Entity<Supervisor>().HasData(supervisors);
-        modelBuilder.Entity<Achievement>().HasData(achiveFaker.Generate(achiveCount));
-        modelBuilder.Entity<CompletedAchievement>().HasData(comletedAchiveFaker.Generate(completedAchiveCount));
+            modelBuilder.Entity<Club>().HasData(clubFaker.Generate(clubsCount));
+            modelBuilder.Entity<User>().HasData(users);
+            modelBuilder.Entity<Admin>().HasData(admins);
+            modelBuilder.Entity<Supervisor>().HasData(supervisors);
+            modelBuilder.Entity<Achievement>().HasData(achiveFaker.Generate(achiveCount));
+            modelBuilder.Entity<CompletedAchievement>().HasData(comletedAchiveFaker.Generate(completedAchiveCount));
+        }
     }
 }
